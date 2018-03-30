a28921d5b1eeabb12338dea070215966bd18f472
Monica Marziano <mmarziano1@gmail.com>
2018-03-30 14:44:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   while number > 0 
     number -= 1 
-    puts "#{number} SECOND(S) !"
+    puts "#{number} SECOND(S)!"
   end
   if number == 0 
     return "HAPPY NEW YEAR!"
